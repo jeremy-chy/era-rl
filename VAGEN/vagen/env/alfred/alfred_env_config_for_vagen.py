085ff928c1996cab663e16b86554dcca59c22df2
jeremy-chy <hc81@illinois.edu>
2025-08-09 23:00:28 -0500

    change data sampling

--- VAGEN/vagen/env/alfred/alfred_env_config_for_vagen.py
+++ VAGEN/vagen/env/alfred/alfred_env_config_for_vagen.py
@@ -1,4 +1,5 @@
 from vagen.env.base.base_env_config import BaseEnvConfig
+import random
 
 class AlfredEnvConfig(BaseEnvConfig):
     def __init__(self, image_mode, image_interval):
@@ -12,13 +13,19 @@ class AlfredEnvConfig(BaseEnvConfig):
     def generate_seeds(self, env_size, train_size):
         import numpy as np
         
-        # train_ranges = [(0,44), (45,50), (300,350), (400,450)]
-        # train_candidates = []
-        # for start, end in train_ranges:
-        #     train_candidates.extend(list(range(start, end)))
-        # train_seeds = np.random.choice(train_candidates, size=train_size, replace=True).tolist()
+        # train_seeds = (list(range(0, 50)) + list(range(300, 350)) + list(range(400, 450))) * 100
+        # train_seeds = train_seeds[:15000]
 
-        train_seeds = list(range(500, 550)) * 100
+        block = list(range(0, 50)) + list(range(300, 350)) + list(range(400, 450))
+        train_seeds = []
+        for _ in range(100):
+            group = block.copy()
+            random.shuffle(group)
+            train_seeds.extend(group)
+
+        print("Generated train seeds:", train_seeds[:10], "...")  # Print first 10 seeds for verification
+
+        # train_seeds = list(range(500, 550)) * 100
         
         # test_ranges = [(100,150), (200,250), (500,550)]
         # test_candidates = []
@@ -26,7 +33,10 @@ class AlfredEnvConfig(BaseEnvConfig):
         #     test_candidates.extend(list(range(start, end)))
         # test_seeds = np.random.choice(test_candidates, size=env_size-train_size, replace=True).tolist()
 
-        #test_seeds = list(range(0, 50)) + list(range(300, 350)) + list(range(400, 450)) + list(range(100, 150)) + list(range(200, 250))
-        test_seeds = list(range(500,550))
+        # test_seeds = list(range(0, 50)) + list(range(300, 350)) + list(range(400, 450)) + list(range(100, 150)) + list(range(200, 250))
+        # test_seeds = list(range(100, 150)) + list(range(200, 250)) + [0]*2
+        # test_seeds = list(range(500,550))
+        test_seeds = list(range(100, 150)) + list(range(200, 250))
+        # test_seeds = list(range(200, 250)) + list(range(100, 150)) + [0]*2
         
         return train_seeds + test_seeds
